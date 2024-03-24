e0f3addc283437adf848abd551ce4cd1bc27f9db
s28111 <s28111@pjwstk.edu.pl>
2024-03-24 11:38:48 +0100

    Siema

---
+++ ObslugaKontenerow/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ObslugaKontenerow
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("Siema");
+        }
+    }
+}
